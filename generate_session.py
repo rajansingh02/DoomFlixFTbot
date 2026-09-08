afa0756ef5d5b3055f4ef3ac29e496eeb8a65dc5
ark <rajansinghraj0@gmail.com>
2026-09-08 21:41:27 +0530

    Deploy DoomFlix FTP bot

---
+++ generate_session.py
@@ -0,0 +1,14 @@
+import os
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+
+with TelegramClient(StringSession(), api_id, api_hash) as client:
+    print("\nYour SESSION_STRING:\n")
+    print(client.session.save())
+    print("\nSession generated successfully.")
